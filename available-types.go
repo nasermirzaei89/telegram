332ae8bb4d9cae1da2ae3aac53c20c2a8f8e386d
Naser Mirzaei <nasermirzaei89@users.noreply.github.com>
2017-08-15 19:23:42 +0430

    Fix voice  filesize type

--- available-types.go
+++ available-types.go
@@ -146,7 +146,7 @@ type Voice struct {
 	FileID   string  `json:"file_id"`
 	Duration int64   `json:"duration"`
 	MimeType *string `json:"mime_type,omitempty"`
-	FileSize *string `json:"file_size,omitempty"`
+	FileSize *int64 `json:"file_size,omitempty"`
 }
 
 // Contact represents a phone contact.
