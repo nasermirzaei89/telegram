eada44dfb9b7d749ef1ba2f4ff23ad9855c5343e
Naser Mirzaei <nasermirzaei89@gmail.com>
2019-06-19 15:52:34 +0430

    implement updating messages methods

--- updating-messages.go
+++ updating-messages.go
@@ -1,43 +1,139 @@
 package telegram
 
 type EditMessageTextResponse interface {
+	Response
+	GetEditedMessage() *Message
+}
+
+type editMessageTextResponse struct {
+	response
+	Result *Message `json:"result,omitempty"`
+}
+
+func (r *editMessageTextResponse) GetEditedMessage() *Message {
+	return r.Result
 }
 
 func (b *bot) EditMessageText(options ...Option) (EditMessageTextResponse, error) {
-	panic("implement me")
+	var res editMessageTextResponse
+	err := doRequest(b.Token, "editMessageText", &res, options...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &res, nil
 }
 
 type EditMessageCaptionResponse interface {
+	Response
+	GetEditedMessage() *Message
+}
+
+type editMessageCaptionResponse struct {
+	response
+	Result *Message `json:"result,omitempty"`
+}
+
+func (r *editMessageCaptionResponse) GetEditedMessage() *Message {
+	return r.Result
 }
 
 func (b *bot) EditMessageCaption(options ...Option) (EditMessageCaptionResponse, error) {
-	panic("implement me")
+	var res editMessageCaptionResponse
+	err := doRequest(b.Token, "editMessageCaption", &res, options...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &res, nil
 }
 
 type EditMessageMediaResponse interface {
+	Response
+	GetEditedMessage() *Message
+}
+
+type editMessageMediaResponse struct {
+	response
+	Result *Message `json:"result,omitempty"`
+}
+
+func (r *editMessageMediaResponse) GetEditedMessage() *Message {
+	return r.Result
 }
 
 func (b *bot) EditMessageMedia(options ...Option) (EditMessageMediaResponse, error) {
-	panic("implement me")
+	var res editMessageMediaResponse
+	err := doRequest(b.Token, "editMessageMedia", &res, options...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &res, nil
 }
 
 type EditMessageReplyMarkupResponse interface {
+	Response
+	GetEditedMessage() *Message
+}
+
+type editMessageReplyMarkupResponse struct {
+	response
+	Result *Message `json:"result,omitempty"`
+}
+
+func (r *editMessageReplyMarkupResponse) GetEditedMessage() *Message {
+	return r.Result
 }
 
 func (b *bot) EditMessageReplyMarkup(options ...Option) (EditMessageReplyMarkupResponse, error) {
-	panic("implement me")
+	var res editMessageReplyMarkupResponse
+	err := doRequest(b.Token, "editMessageReplyMarkup", &res, options...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &res, nil
 }
 
 type StopPollResponse interface {
+	Response
+	GetStoppedPoll() *Poll
+}
+
+type stopPollResponse struct {
+	response
+	Result *Poll `json:"result,omitempty"`
+}
+
+func (r *stopPollResponse) GetStoppedPoll() *Poll {
+	return r.Result
 }
 
 func (b *bot) StopPoll(options ...Option) (StopPollResponse, error) {
-	panic("implement me")
+	var res stopPollResponse
+	err := doRequest(b.Token, "stopPoll", &res, options...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &res, nil
 }
 
 type DeleteMessageResponse interface {
+	Response
+}
+
+type deleteMessageResponse struct {
+	response
 }
 
 func (b *bot) DeleteMessage(options ...Option) (DeleteMessageResponse, error) {
-	panic("implement me")
+	var res deleteMessageResponse
+	err := doRequest(b.Token, "deleteMessage", &res, options...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &res, nil
 }
