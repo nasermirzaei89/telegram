3ddf6978ce43d4a174d544389045a994bc4cd08f
Naser Mirzaei <nasermirzaei89@gmail.com>
2021-11-26 02:22:30 +0330

    Add MessageEntityType to MessageEntity.Type

--- available-types.go
+++ available-types.go
@@ -101,14 +101,34 @@ type MessageID struct {
 	MessageID int `json:"message_id"`
 }
 
+type MessageEntityType string
+
+const (
+	MessageEntityTypeMention       MessageEntityType = "mention"
+	MessageEntityTypeHashtag       MessageEntityType = "hashtag"
+	MessageEntityTypeCashtag       MessageEntityType = "cashtag"
+	MessageEntityTypeBotCommand    MessageEntityType = "bot_command"
+	MessageEntityTypeURL           MessageEntityType = "url"
+	MessageEntityTypeEmail         MessageEntityType = "email"
+	MessageEntityTypePhoneNumber   MessageEntityType = "phone_number"
+	MessageEntityTypeBold          MessageEntityType = "bold"
+	MessageEntityTypeItalic        MessageEntityType = "italic"
+	MessageEntityTypeUnderline     MessageEntityType = "underline"
+	MessageEntityTypeStrikethrough MessageEntityType = "strikethrough"
+	MessageEntityTypeCode          MessageEntityType = "code"
+	MessageEntityTypePre           MessageEntityType = "pre"
+	MessageEntityTypeTextLink      MessageEntityType = "text_link"
+	MessageEntityTypeTextMention   MessageEntityType = "text_mention"
+)
+
 // MessageEntity struct.
 type MessageEntity struct {
-	Type     string  `json:"type"`
-	Offset   int     `json:"offset"`
-	Length   int     `json:"length"`
-	URL      *string `json:"url,omitempty"`
-	User     *User   `json:"user,omitempty"`
-	Language string  `json:"language,omitempty"`
+	Type     MessageEntityType `json:"type"`
+	Offset   int               `json:"offset"`
+	Length   int               `json:"length"`
+	URL      *string           `json:"url,omitempty"`
+	User     *User             `json:"user,omitempty"`
+	Language string            `json:"language,omitempty"`
 }
 
 // PhotoSize struct.
